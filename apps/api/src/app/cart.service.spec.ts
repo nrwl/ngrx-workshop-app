60f61ce21d77e408c944267d6adee0256228856a
Wes Grimes <wesgrimes@gmail.com>
2019-07-15 19:05:21 -0400

    Remove incorrect test

--- apps/api/src/app/cart.service.spec.ts
+++ apps/api/src/app/cart.service.spec.ts
@@ -12,47 +12,5 @@ describe('CartService', () => {
     service = app.get<CartService>(CartService);
   });
 
-  describe('getShippingMethods', () => {
-    it('should return mock shipping methods', () => {
-      expect(service.getShippingMethods()).toEqual([
-        {
-          type: 'Overnight',
-          price: 25.99
-        },
-        {
-          type: '2-Day',
-          price: 9.99
-        },
-        {
-          type: 'Postal',
-          price: 2.99
-        }
-      ]);
-    });
-  });
-
-  describe('getProducts', () => {
-    it('should return mock products', () => {
-      expect(service.getProducts()).toEqual([
-        {
-          productId: 1,
-          name: 'Phone XL',
-          price: 799,
-          description: 'A large phone with one of the best screens'
-        },
-        {
-          productId: 2,
-          name: 'Phone Mini',
-          price: 699,
-          description: 'A great phone with one of the best cameras'
-        },
-        {
-          productId: 3,
-          name: 'Phone Standard',
-          price: 299,
-          description: ''
-        }
-      ]);
-    });
-  });
+  // TODO: Build out tests for each method
 });
