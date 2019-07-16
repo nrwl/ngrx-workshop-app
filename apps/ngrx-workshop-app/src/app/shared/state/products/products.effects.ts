93eed329e93c1afe18ead9f4dcf57e9a76f196d1
Brandon Roberts <robertsbt@gmail.com>
2019-07-16 12:40:42 -0500

    feat: add state for products feature

--- apps/ngrx-workshop-app/src/app/shared/state/products/products.effects.ts
+++ apps/ngrx-workshop-app/src/app/shared/state/products/products.effects.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@angular/core';
-import { exhaustMap, map, catchError } from 'rxjs/operators';
-import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { ProductService } from '@ngrx-workshop-app/product-data-access';
-
-import * as ProductsActions from './products.actions';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
+import * as ProductsActions from './products.actions';
 
 @Injectable()
 export class ProductsEffects {
-  loadProducts$ = createEffect(() => this.actions$.pipe(
-    ofType(
-      ProductsActions.enterProductsPage,
-      ProductsActions.enterProductDetailsPage
-    ),
-    exhaustMap(() =>
-      this.productsService.getProducts().pipe(
-        map(products => ProductsActions.loadProductsSuccess({ products })),
-        catchError(() =>
-          of(
-            ProductsActions.loadProductsFailure({
-              error: 'Unable to load products'
-            })
+  loadProducts$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(
+        ProductsActions.enterProductsPage,
+        ProductsActions.enterProductDetailsPage
+      ),
+      exhaustMap(() =>
+        this.productsService.getProducts().pipe(
+          map(products => ProductsActions.loadProductsSuccess({ products })),
+          catchError(() =>
+            of(
+              ProductsActions.loadProductsFailure({
+                error: 'Unable to load products'
+              })
+            )
           )
         )
       )
     )
-  ));
+  );
 
   constructor(
     private actions$: Actions,
