5f3b0b957e9680341f12df205dc7a2186bd47fe6
Jack Hsu <jack.hsu@gmail.com>
2020-08-12 21:10:23 -0400

    chore: update to Angular 10

--- libs/api-interface/jest.config.js
+++ libs/api-interface/jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   name: 'api-interface',
   preset: '../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/api-interface'
+  coverageDirectory: '../../coverage/libs/api-interface',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
